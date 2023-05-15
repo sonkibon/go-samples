6038e607d953ae3f593b04fbf553d237e9902939
Kwibong Song <kwibong0128@gmail.com>
2023-05-15 20:09:22 +0900

    feat: add sample code of marshaling a struct that field is not exported

--- yaml/main.go
+++ yaml/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"gopkg.in/yaml.v3"
 )
+
+// marshalUnexported is a struct to marshal YAML documents that do not have exported struct fields.
+type marshalUnexported struct {
+	b           bool
+	i           int
+	s           string
+	intSlice    []int
+	stringSlice []string
+}
 
 // marshal is a function that wraps the Marshal function from "gopkg.in/yaml.v3
 func marshal(in interface{}) ([]byte, error) {
@@ -16,4 +26,26 @@ func marshal(in interface{}) ([]byte, error) {
 	return out, nil
 }
 
-func main() {}
+func main() {
+	mu := marshalUnexported{
+		b: true,
+		i: 10,
+		s: "hoge",
+		intSlice: []int{
+			1,
+			2,
+			3,
+		},
+		stringSlice: []string{
+			"aaa",
+			"bbb",
+			"ccc",
+		},
+	}
+
+	muOut, err := marshal(&mu)
+	if err != nil {
+		log.Fatalf("encode: %v", err)
+	}
+	fmt.Printf("Result of marshaling a struct that field is not exported\n%s\n", string(muOut))
+}
