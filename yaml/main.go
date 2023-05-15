5cd449e9f44809194acb47f84e88cb43d0255b00
Kwibong Song <kwibong0128@gmail.com>
2023-05-15 20:08:05 +0900

    feat: add marshal function

--- yaml/main.go
+++ yaml/main.go
@@ -1,3 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+// marshal is a function that wraps the Marshal function from "gopkg.in/yaml.v3
+func marshal(in interface{}) ([]byte, error) {
+	out, err := yaml.Marshal(in)
+	if err != nil {
+		return nil, fmt.Errorf("yaml.Marshal: %w", err)
+	}
+
+	return out, nil
+}
+
 func main() {}
