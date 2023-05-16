03e45bf4b14c9835f91eae36fc23e2c3d0d55dcc
Kwibong Song <kwibong0128@gmail.com>
2023-05-16 11:34:29 +0900

    feat: define Topic and Subscription struct

--- pubsub/sns.go
+++ pubsub/sns.go
@@ -1,1 +1,16 @@
 package pubsub
+
+// Topic provides a PubsubClient for a specific topic.
+type Topic struct {
+	client    *PubsubClient
+	topicName string
+	topicArn  string
+}
+
+// Subscription provides a PubsubClient for a specific subscription.
+type Subscription struct {
+	client          *PubsubClient
+	subscriptionArn string
+	topic           Topic
+	queue           Queue
+}
