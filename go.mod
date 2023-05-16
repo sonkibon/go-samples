0f6048b1e6524871cb5821be2ef5c9f531d08074
Kwibong Song <kwibong0128@gmail.com>
2023-05-16 11:29:15 +0900

    feat: implement consume method

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.18.0
 	github.com/aws/aws-sdk-go-v2/service/sns v1.20.11
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.22.0
+	golang.org/x/sync v0.2.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
--- pubsub/sqs.go
+++ pubsub/sqs.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"golang.org/x/sync/errgroup"
 )
 
 // Queue provides a PubsubClient for a specific queue.
@@ -41,5 +42,63 @@ func (q *Queue) Send(ctx context.Context, message string, attributes map[string]
 	}
 
 	log.Default().Printf("message id: %s", *m.MessageId)
+	return nil
+}
+
+// consume receives a message from a specific queue and executes the argument f function to delete the message.
+// It can also retry by changing the visibility timeout of the specified message in the queue to a new value.
+func (q *Queue) consume(ctx context.Context, f func(context.Context, types.Message) (bool, error)) error {
+	params := &sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(q.queueUrl),
+		MaxNumberOfMessages: q.client.Config.MaxNumberOfMessages,
+		WaitTimeSeconds:     q.client.Config.WaitTimeSeconds,
+	}
+
+	output, err := q.client.SQS.ReceiveMessage(ctx, params)
+	if err != nil {
+		return fmt.Errorf("q.SQS.ReceiveMessage: %w", err)
+	}
+
+	group, _ := errgroup.WithContext(ctx)
+	for _, message := range output.Messages {
+		group.Go(func(m types.Message) func() error {
+			return func() error {
+				retryable, err := f(ctx, m)
+
+				if err == nil {
+					if _, err := q.client.SQS.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+						QueueUrl:      aws.String(q.queueUrl),
+						ReceiptHandle: m.ReceiptHandle,
+					}); err != nil {
+						return fmt.Errorf("q.SQS.DeleteMessage: %w", err)
+					}
+
+					return nil
+				}
+
+				if retryable {
+					if _, err := q.client.SQS.ChangeMessageVisibility(ctx, &sqs.ChangeMessageVisibilityInput{
+						QueueUrl:          aws.String(q.queueUrl),
+						ReceiptHandle:     m.ReceiptHandle,
+						VisibilityTimeout: q.client.Config.RequeueVisibilityTimeout,
+					}); err != nil {
+						return fmt.Errorf("q.SQS.ChangeMessageVisibility: %w", err)
+					}
+				} else {
+					if _, err := q.client.SQS.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+						QueueUrl:      aws.String(q.queueUrl),
+						ReceiptHandle: m.ReceiptHandle,
+					}); err != nil {
+						return fmt.Errorf("q.SQS.DeleteMessage: %w", err)
+					}
+				}
+				return nil
+			}
+		}(message))
+	}
+	if err := group.Wait(); err != nil {
+		return fmt.Errorf("group.Wait: %w", err)
+	}
+
 	return nil
 }
